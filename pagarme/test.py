47c07b434c447c8cb43a1581d92d1bf63db170a9
Luís Felipe <lfa.luisfelipe@gmail.com>
2017-05-21 20:09:03 -0300

    Card

--- pagarme/test.py
+++ pagarme/test.py
@@ -1,4 +1,5 @@
 from transaction import Transaction
+from card import Card
 import pprint
 
 ###
@@ -6,4 +7,8 @@ import pprint
 ###
 #pprint.pprint (Transaction().split_rule(1551520))
 
-pprint.pprint (Transaction().refund(1551520))
+###
+# Card
+###
+# create pprint.pprint (Card().create(data={'card_hash':'580404_I1BCMNhpekqfLbsbjQSSviDNYexbKPgp9S6Dc+/jL32jsrilaxJJ8GA8hvaRpAgkgaB8ogM6llxQktNRxwVwcJPxS7vZQJbKNoByOMvUe9he2HUmWdIid2vrMP3Hr1ap+xFTEvdetKLlC/6+dnlCZrha7Fodo6WrwH98A5s0ahOPfUslRZkUprGwwdTvSmVXOl642jVnhvNsYrsuEphuERo4Ut1ZSz86md/O8xCSlEcNyo8Wg7JVzo/A0cniR3RMUA9ggZ3NDkJz8psOytlWjsSr1PFY7tf2ijnSldN0CxMJMcvL5zeretwqO+rUWCN1tW+ZKnnxYrZ1qH2/L9maUg=='}))
+# find pprint.pprint (Card().find('card_cj2zaz3jj00x5d76e5i95ujrk'))
